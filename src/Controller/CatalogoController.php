6b68a72746d798b94cad197bfd7c2384f6c3b272
Hector <zeldax54@gmail.com>
2021-01-17 01:35:08 -0600

    mailer change $mailer

--- src/Controller/CatalogoController.php
+++ src/Controller/CatalogoController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Cabana;
 
 class CatalogoController extends AbstractController
 {
@@ -15,16 +16,33 @@ class CatalogoController extends AbstractController
     */
     public function redirectIndex():Response
     {
-        return $this->redirect($this->generateUrl('cabanas_index'));
+        return $this->redirect($this->generateUrl('cabanas_index',array('nombre'=>54)));
     }
 
     /**
-     * @Route("/cabanas", name="cabanas_index")
+     * @Route("/cabanas/{nombre}",options={"expose"=true}, name="cabanas_index")
      */
-    public function cabanasIndex(): Response
-    {
+    public function cabanasIndex($nombre=null): Response
+    {        
+        $search = array('desactivado'=>false);     
+        if($nombre!=null)
+           $search['nombre']=$nombre;
+        $cabanas = $this->getDoctrine()->getRepository(Cabana::class)->findBy($search);
         return $this->render('frontpages/cabanas.html.twig', [
-            'controller_name' => 'CatalogoController',
+
+            'cabanas' => $cabanas,
+        ]);
+    }
+
+     /**
+     * @Route("/cabana/{id}", name="cabana_detail",options={"expose"=true}, methods={"GET"})
+     */
+    public function cabanaDetail(Cabana $cabana): Response
+    {        
+
+  
+        return $this->render('frontpages/cabanadetail.html.twig', [
+            'cabana' => $cabana,
         ]);
     }
 }
